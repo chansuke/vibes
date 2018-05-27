49f126d3d2f41723eb9fe9fffe6a64f756ae7fd1
chansuke <chansuke@georepublic.de>
2018-05-27 14:39:23 +0900

    Add eslint & prettier

--- src/token.js
+++ src/token.js
@@ -2,13 +2,13 @@ const TokenType = {
   String: 'string',
   Integer: 'integer',
   Identifier: 'identifier'
-};
+}
 
 class Token {
   constructor(type, value, line, column) {
-    this.type = type;
-    this.value = value;
-    this.line = line;
-    this.column = column;
+    this.type = type
+    this.value = value
+    this.line = line
+    this.column = column
   }
 }
